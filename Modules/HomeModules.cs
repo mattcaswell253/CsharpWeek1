4d0f40235cc82302d60ba70492fb7e827fb8189d
mattcaswell <matthewcaswell253@Gmail.com>
2017-02-10 09:32:52 -0800

    add contact class and homemodule page

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using System.Collections.Generic;
+using AdressBook.Objects;
+
+namespace AddressBookApp
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
+  }
+}
---
+++ Objects/Contact.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone-number;
+    private string _address;
+    private int _id;
+
+    private static List<Contact> _contactList = new List<Contact> {};
+
+    public Contact(string newName, string newPhoneNumber,
+    string newAddress)
+    {
+      _name = newName;
+      _phone-number = newPhoneNumber;
+      _address = newAddress;
+      _contactList.Add(this);
+      _id = _contactList.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhoneNumber()
+    {
+      return _phone-number
+    }
+
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phone-number = newPhoneNumber;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Contact> GetAll()
+    {
+      return _contactList;
+    }
+
+    public static void ClearAll()
+    {
+      _contactList.Clear();
+    }
+
+    public static Contact Find(int searchId)
+    {
+      return _contactList[searchId-1];
+    }
+  }
+}
